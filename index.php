19eab3d265e57daf22902e6425f9636a99cee241
Shaxens <arthurvietti@gmail.com>
2022-04-09 21:08:01 +0200

    Début du front, carousel + background

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>FlexCort</title>
+</head>
+
+<body>
+    
+    <div id="bk">
+        <img class="img-responsive" src="./images/background.jpg" alt="Responsive image">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>FlexCort</h1>
+                <h3>Parce que commander une escort n'a jamais été aussi simple</h3>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" id="carouselGlobal">
+        <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+            <div class="row">
+
+                <div class="">
+                        <div id="carousel">
+                            <div id="containerCarousel"></div>
+                            <img src="./images/fleche.jpg" alt="" class="bouton" id="d">
+                            <img src="./images/fleche.jpg" alt="" class="bouton" id="g">
+                        </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm"></div>
+    </div>
+    </div>
+
+    <script src="js\main.js"></script>
+</body>
+
+</html>
