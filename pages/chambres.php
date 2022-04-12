2a4f3ca0989c9fa8f3a5931cca3eaf58d656b787
Shaxens <arthurvietti@gmail.com>
2022-04-12 15:41:00 +0200

    Ajout date formulaire et fix du favicon + navbar

--- pages/chambres.php
+++ pages/chambres.php
@@ -6,12 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../style/style.css">
+    <link rel="shortcut icon" type="image/png" href="../images/favicon.ico"/>
+
     <title>Nos chambres</title>
 </head>
 <body>
+    <?php include("navbar.php") ?>
     <div id="bk">
         <img class="img-responsive" src="../images/background.jpg" alt="Responsive image">
     </div>
-    <?php include("navbar.php") ?>
 </body>
 </html>
--- pages/profil.php
+++ pages/profil.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../style/style.css">
+    <link rel="shortcut icon" type="image/png" href="../images/favicon.ico"/>
     <title>Nos chambres</title>
 </head>
 <body>
-
     <?php include("navbar.php") ?>
+
 <br>
 <br>
 <br>
@@ -22,26 +23,31 @@
                 <div class="row">
                 <div class="col">
                     <label for="prenom">Prénom</label>
-                    <input type="text" class="form-control" placeholder="Jacques" id="prenom">
+                    <input type="text" class="form-control" placeholder="Jacques" id="prenom" required>
                 </div>
                 <div class="col">
                     <label for="nom">Nom</label>
-                    <input type="text" class="form-control" placeholder="Dupont" id="nom">
+                    <input type="text" class="form-control" placeholder="Dupont" id="nom" required>
                 </div>
                 </div>
                 <br>
                 <div class="mb-3">
                     <label for="email" class="form-label">Adresse mail</label>
-                    <input type="email" class="form-control" id="email">
+                    <input type="email" class="form-control" id="email" required>
                     <div id="emailHelp" class="form-text">Nous ne partagerons pas votre adresse mail.</div>
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Mot de passe </label>
-                    <input type="password" class="form-control" id="password">
+                    <input type="password" class="form-control" id="password" required>
                 </div>
                 <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="checkbox">
+                    <input type="checkbox" class="form-check-input" id="checkbox" required>
                     <label class="form-check-label" for="checkbox">J'accepte les conditions d'utilisations et de ventes</label>
+                </div>
+                <div class="mb-3">
+                    <label for="date">Entrez votre date de naissance : </label>
+
+                    <input type="date" id="date" name="date" value="" required>
                 </div>
                 <button type="submit" class="btn btn-primary">Envoyer</button>
             </form>
