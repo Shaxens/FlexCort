b48f0e4fbeb27cabf4cf04094b11e353d1b2c88c
DrYruo <oury.jordan@hotmail.com>
2022-04-18 22:12:26 +0200

    ajout du getUtilisateur()

--- pages/classes/TEST.php
+++ pages/classes/TEST.php
@@ -1,14 +1,9 @@
 <?PHP
 require_once 'UtilisateurManager.php';
 
-$utilisaTest = new Utilisateur('1234567891', 'Lourd', 'Jesus', 'filsDeDieu@god.win', 'trololo');
-echo 'id = ', $utilisaTest->getId(), '<br>';
-echo 'nom = ', $utilisaTest->getNom(), '<br>';
-echo 'prenom = ', $utilisaTest->getPrenom(), '<br>';
-echo 'mail = ', $utilisaTest->getMail(), '<br>';
-echo 'mdp = ', $utilisaTest->getMdp(), '<br>';
-
 $utilisateurManager = new UtilisateurManager();
 
-$utilisateurManager->add($utilisaTest);
-;
+$utilisatest = $utilisateurManager->getUtilisateur('bxl@enForce.be');
+echo $utilisatest->getNom();
+echo $utilisatest->getPrenom();
+
--- pages/classes/UtilisateurManager.php
+++ pages/classes/UtilisateurManager.php
@@ -10,28 +10,53 @@ class UtilisateurManager {
     public function __construct()
     {
         $this->connexionBdd = new Bdd;
+        echo 'utilisateurManager INITIALISE','<br>';
     }
 
     // METHODES
-    public function add(Utilisateur $utilisateur)
+    public function addUtilisateur(Utilisateur $utilisateur)
     {
-        try {
-            echo '<br>', '...Ajout de l\'utilisateur ' , $utilisateur->getPrenom(), ' à la base de donné EN COURS... ';
+        try
+        {
             $sql = "INSERT INTO UTILISATEUR(nom, prenom, mail, mdp) VALUES (?, ?, ?, ?)";
-            $req = $this->connexionBdd->preparerRequete($sql);
-            echo 'requete préparée !';
 
+            $req = $this->connexionBdd->preparerRequete($sql);
             $req->bindValue(1, $utilisateur->getNom(), PDO::PARAM_STR);
             $req->bindValue(2, $utilisateur->getPrenom(), PDO::PARAM_STR);
             $req->bindValue(3, $utilisateur->getMail(), PDO::PARAM_STR);
             $req->bindValue(4, $utilisateur->getMdp(), PDO::PARAM_STR);
-
             $req->execute();
-            echo '<br>', ' ... Ajout de l\'utilisateur : ', $utilisateur->getPrenom(), ' TERMINE...';
+
+            echo $utilisateur->getNom(), ' ', $utilisateur->getPrenom(), ' ajouté avec succès';
         }
         catch (PDOException $e)
         {
-            echo 'ERREUR utilisateurManager->add(Utilisateur ',$utilisateur->getPrenom(),')','<br>'.$e->getMessage();
+            echo 'ERREUR addUtilisateur'.$e->getMessage();
         }
+    }
+
+    public function getUtilisateur(string $mail): Utilisateur|int
+    {
+        try
+        {
+            $sql = "SELECT mail, mdp, nom, prenom FROM UTILISATEUR WHERE UTILISATEUR.mail = ?";
+
+            $req = $this->connexionBdd->preparerRequete($sql);
+            $req->bindValue(1, $mail, PDO::PARAM_STR);
+            $req->execute();
+            $resultat = $req->fetch(PDO::FETCH_OBJ);
+
+            return new Utilisateur($resultat->nom, $resultat->prenom, $resultat->mail, $resultat->mdp);
+        }
+        catch (PDOException $e)
+        {
+            echo 'ERREUR getUtilisateur'.$e->getMessage();
+            return -1;
+        }
+    }
+
+    public function deleteUtilisateur(Utilisateur $utilisateur)
+    {
+
     }
 }
