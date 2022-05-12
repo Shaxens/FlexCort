2e54de98dce76e7c282547e5c34625c6575e6d6a
Shaxens <arthurvietti@gmail.com>
2022-05-13 01:05:06 +0200

    Ajout cards modèles et créations pages modèles

--- view/pages/modeles.php
+++ view/pages/modeles.php
@@ -12,7 +12,84 @@
 <body>
 <?php include("navbar.php") ?>
 
-<div class="container-fluid colBk">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-xxl-2">
+            <br><br><br><br><br>
+            <h1>Nos modèles</h1><hr>
+        </div>
+        <div class="col"></div>
+    </div>
+
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="../images/modeles/blackWindow.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Black Window</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="./modeles/blackWindow.php" class="btn btn-primary">Réservez-moi</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="../images/modeles/princessePeach.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Princesse Peach</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="./modeles/princessePeach.php" class="btn btn-primary">Réservez-moi</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="../images/modeles/microman.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Microman</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="./modeles/microman.php" class="btn btn-primary">Réservez-moi</a>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="../images/modeles/randomGoodGuy.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Random GoodGuy</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="./modeles/randomGoodGuy.php" class="btn btn-primary">Réservez-moi</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="../images/modeles/spartatouze.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Spartatouze</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="./modeles/spartatouze.php" class="btn btn-primary">Réservez-moi</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="../images/modeles/rocoSifredi.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Roco & Sifredi</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="./modeles/rocoSifredi.php" class="btn btn-primary">Réservez-nous</a>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
     </div>
 </body>
 </html>
---
+++ view/pages/modeles/blackWindow.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../style/style.css">
+    <link rel="shortcut icon" type="image/png" href="../images/favicon.ico"/>
+    <title>Document</title>
+</head>
+<body>
+<?php include("navbar.php") ?>
+    <div class="container-fluid">
+        <br><br><br>
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-3">
+                <img src="../../../view/images\modeles\blackWindow.jpg" alt="" style="width: 250px;">
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col">
+                        <p>Black Window</p>
+                    </div>
+                    <div class="col"></div>
+                </div>
+            </div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
+    </div>
+</body>
+</html>
