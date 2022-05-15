5c43f48f846382273a41845dd829596327401c4f
DrYruo <oury.jordan@hotmail.com>
2022-05-16 01:35:04 +0200

    voila la liste des modeles jeune homme !

--- controler/pageModeleControler.php
+++ controler/pageModeleControler.php
@@ -4,6 +4,5 @@ require_once __DIR__ . './../model/includeModel.php';
 $modeleManager = new ModeleManager();
 $tableau = $modeleManager->creerListeModeleFormatJson();
 
-header('Content-Type: application/json');
 echo $tableau;
 exit;
