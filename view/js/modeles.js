133d00aae155c5e014272fb8a68c5005dfe6656b
Shaxens <arthurvietti@gmail.com>
2022-05-16 01:40:20 +0200

    ça marche !

--- view/js/modeles.js
+++ view/js/modeles.js
@@ -4,16 +4,73 @@ afficherCard.innerHTML = ``;
 
 
 window.onload = function() {
-        fetch('../../controler/pageModeleControler.php', {
+    fetch('../../controler/pageModeleControler.php', {
             method: "GET",
-            headers: {"Content-type": "application/json; charset=UTF-8"}
+            headers: { "Content-type": "application/json; charset=UTF-8" }
         })
-            .then(response => response.json())
-            .then(function(tableauModeles)
-            {
-                for (modele of tableauModeles) {
-                    console.log(modele);
+        .then(response => response.json())
+        .then(function(tableauModeles) {
+            for (modele of tableauModeles) {
+                console.log(modele);
+                if (modele.length == 1) {
+                    let modeleCard3 = `
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col">
+                <div class="card zoomCard" style="width: 18rem;" name="${modele.idModele}">
+                    <a type="button" href="./modeles/${modele.pseudo}.php">
+                    <img src="../images/modeles/${modele.pseudo}.jpg" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <h5 class="card-title">${modele.pseudo}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="./modeles/${modele.pseudo}.php" class="btn btn-primary">Réservez-moi</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+                    afficherCard.innerHTML += modeleCard3;
                 }
-            })
-            .catch(erreur => console.log(erreur))
+                let modeleCard = `
+    <div class="col">
+        <div class="card zoomCard" style="width: 18rem;" name="${modele.idModele}">
+            <a type="button" href="./modeles/${modele.pseudo}.php">
+            <img src="../images/modeles/${modele.pseudo}.jpg" class="card-img-top" alt="...">
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${modele.pseudo}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="./modeles/${modele.pseudo}.php" class="btn btn-primary">Réservez-moi</a>
+            </div>
+        </div>
+    </div>
+    `;
+                if (modele % 3 == 0) {
+                    let modeleCard3 = `
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col">
+                <div class="card zoomCard" style="width: 18rem;" name="${modele.idModele}">
+                    <a type="button" href="./modeles/${modele.pseudo}.php">
+                    <img src="../images/modeles/${modele.pseudo}.jpg" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <h5 class="card-title">${modele.pseudo}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="./modeles/${modele.pseudo}.php" class="btn btn-primary">Réservez-moi</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+                    afficherCard.innerHTML += modeleCard3;
+                } else {
+                    afficherCard.innerHTML += modeleCard;
+                }
+                afficherCard.innerHTML;
+            }
+
+        })
+        .catch(erreur => console.log(erreur))
 }
