27481acf7033f584ed1f5416252bdf9db8391293
Shaxens <arthurvietti@gmail.com>
2022-05-16 01:34:55 +0200

    Pas besoin

--- view/js/test.js
+++ view/js/test.js
@@ -1,13 +1,59 @@
-function test() {
-    const getPhp = "../../model/entityManager/ModeleManager.php"
-
-    fetch(getPhp, {
-        method: "GET",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-    var result = "<?php getAllModele(); ?>";
-    alert(result);
-    return false;
-}
+// for (modele of listeModeles) {
+//     if (modele.length == 1) {
+//         let modeleCard3 = `
+//         <div class="row">
+//             <div class="col-3"></div>
+//             <div class="col">
+//                 <div class="card zoomCard" style="width: 18rem;" name="${modele.pseudo}">
+//                     <a type="button" href="./modeles/${modele.pseudo}.php">
+//                     <img src="../images/modeles/${modele.pseudo}.jpg" class="card-img-top" alt="...">
+//                     </a>
+//                     <div class="card-body">
+//                         <h5 class="card-title">${modele.pseudo}</h5>
+//                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                         <a href="./modeles/${modele.pseudo}.php" class="btn btn-primary">Réservez-moi</a>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//         `;
+//         document.afficherCard.innerHTML += modeleCard3;
+//     }
+//     let modeleCard = `
+//     <div class="col">
+//         <div class="card zoomCard" style="width: 18rem;" name="${modele.pseudo}">
+//             <a type="button" href="./modeles/${modele.pseudo}.php">
+//             <img src="../images/modeles/${modele.pseudo}.jpg" class="card-img-top" alt="...">
+//             </a>
+//             <div class="card-body">
+//                 <h5 class="card-title">${modele.pseudo}</h5>
+//                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                 <a href="./modeles/${modele.pseudo}.php" class="btn btn-primary">Réservez-moi</a>
+//             </div>
+//         </div>
+//     </div>
+//     `;
+//     if (modele % 3 == 0) {
+//         let modeleCard3 = `
+//         <div class="row">
+//             <div class="col-3"></div>
+//             <div class="col">
+//                 <div class="card zoomCard" style="width: 18rem;" name="${modele.pseudo}">
+//                     <a type="button" href="./modeles/${modele.pseudo}.php">
+//                     <img src="../images/modeles/${modele.pseudo}.jpg" class="card-img-top" alt="...">
+//                     </a>
+//                     <div class="card-body">
+//                         <h5 class="card-title">${modele.pseudo}</h5>
+//                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                         <a href="./modeles/${modele.pseudo}.php" class="btn btn-primary">Réservez-moi</a>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//         `;
+//         document.afficherCard.innerHTML += modeleCard3;
+//     } else {
+//         document.afficherCard.innerHTML += modeleCard;
+//     }
+//     document.afficherCard.innerHTML;
+// }
